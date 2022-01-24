de122b1b8de4a0342e583e26f865d9a80e52a3db
Dabvexx <alexanderwoods2019@gmail.com>
2022-01-24 11:32:15 -0500

    yeah
    true

--- Assets/SuperTiled2Unity/Import/GrassTileSet.tsx
+++ Assets/SuperTiled2Unity/Import/GrassTileSet.tsx
@@ -4,4 +4,19 @@
  <terraintypes>
   <terrain name="Walls" tile="-1"/>
  </terraintypes>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.125" y="7.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.375" y="8.25" width="7.55" height="7.55"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="7.875" y="7.625"/>
+  </objectgroup>
+ </tile>
 </tileset>
