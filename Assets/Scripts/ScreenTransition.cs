d46fdece3cb53441fb76aa482cd6f17c1c473a53
DaVexx <alexanderwoods2019@gmail.com>
2022-01-24 22:47:06 -0500

    Added Scene Picker
    True

--- Assets/Scripts/ScreenTransition.cs
+++ Assets/Scripts/ScreenTransition.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class ScreenTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(GetComponent<ScenePicker>().scenePath);
     }
 }
