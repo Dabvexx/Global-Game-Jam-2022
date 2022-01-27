7782dcbfe354bae38e2dec6590faa94dc698b616
Alex Woods <alexanderwoods2019@gmail.com>
2022-01-26 21:35:28 -0500

    Made a few possibly pointless edits lol
    yeah i thought of a better system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D rb2D;
+
+    [SerializeField]
+    private float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +17,17 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            //transform.Translate(Vector2.right * speed * Time.deltaTime);
+            rb2D.velocity = new Vector2(1 * speed, rb2D.velocity.y);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb2D.velocity = new Vector2(-1 * speed, rb2D.velocity.y);
+        }
     }
 }
---
+++ Assets/Scripts/ScreenTransitionManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTransitionManager : MonoBehaviour
+{
+    #region Variables
+    GameObject[] exits;
+    #endregion
+
+    #region Unity Methods
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+}
